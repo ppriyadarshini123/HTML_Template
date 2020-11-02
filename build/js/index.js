fe2009ed74fd0ef561823627f3a415972aa1010a
Payal <payal.priyadarshini@gmail.com>
2020-11-02 11:36:27 +0000

    Before making it responsive

--- build/js/index.js
+++ build/js/index.js
@@ -1,81 +1,88 @@
 /*
-* @file index.js
-* @folder build/js
-*/
+ * @file index.js
+ * @folder build/js
+ */
 /*
------------------------------------------------
-1) 	create your iffy
-2) 	fetch DOM elements and store them into const variables
-2.1) 	store other values into let variables
-3) 	set up your initialising pattern
-4) 	subscribe handlers to events on elements
-5) 	implement handlers
------------------------------------------------
-*/
+ -----------------------------------------------
+ 1) 	create your iffy
+ 2) 	fetch DOM elements and store them into const variables
+ 2.1) 	store other values into let variables
+ 3) 	set up your initialising pattern
+ 4) 	subscribe handlers to events on elements
+ 5) 	implement handlers
+ -----------------------------------------------
+ */
 
-(function(){
+(function () {
 
 
-    /*const submit = document.getElementsByClassName("submitBtn");
-    const $menuIcon = $('#openNav');*/
-   
+    /*const submit = document.getElementsByClassName("submitBtn");*/
+    const $divFloorPlan = $('#divFloorPlan');
+    const $clickedDivFloorPlan = $('#clickedDivFloorPlan');
+    const $clickedDivDetails = $('#clickedDivDetails');
+    const $divDetails = $('#divDetails');
+    
     
     /* FUNCTIONS*/
-    
-     /**
+
+    /**
      * @name divFloorPlanClicked
      * @desc Handling clicking of floor plan divs
      */
     function divFloorPlanClicked() {
-        $("#divFloorPlan").click(function(e){  
-            e.preventDefault()
-            $("#clickedDivFloorPlan").show();
-            $("#clickedDivDetails").hide();
-            $("#divFloorPlan").addClass("background-color", "#358016");
-            $("#divFloorPlan").addClass("color", "#FFFFFF");
-            $("#divDetails").addClass("background-color", "#1CE823");
-            $("#divDetails").addClass("color", "#FEFF1F");
-        });//click        
+        $(document).ready(function () {
+            $("#divFloorPlan").click(function (e) {
+                e.preventDefault();
+                $clickedDivFloorPlan.show();
+                $clickedDivDetails.hide();
+                $divFloorPlan.addClass("clicked");
+                $divFloorPlan.removeClass("notClicked");
+                $divDetails.addClass("notClicked");
+                $divDetails.removeClass("clicked");
+            }); //click      
+        });//document.ready
     }//divFloorPlanClicked
-    
-    
+
+
     /**
      * @name divDetailsClicked
      * @desc Handling clicking of details divs
      */
-    function divDetailsClicked() {        
-        $("#divDetails").click(function(){
-            $("#clickedDivFloorPlan").hide();
-            $("#clickedDivDetails").show();
-            $("#divDetails").addClass("background-color", "#358016");
-            $("#divDetails").addClass("color", "#FFFFFF");
-            $("#divFloorPlan").addClass("background-color", "#1CE823");
-            $("#divFloorPlan").addClass("color", "#FEFF1F");
-        });//click        
+    function divDetailsClicked() {
+        $(document).ready(function () {
+            $divDetails.click(function (e) {
+                e.preventDefault();
+                $clickedDivFloorPlan.hide();
+                $clickedDivDetails.show();
+                $divDetails.addClass("clicked");
+                $divDetails.removeClass("notClicked");
+                $divFloorPlan.addClass("notClicked");
+                $divFloorPlan.removeClass("clicked");
+            }); //click      
+        });//document.ready
     }//divDetailsClicked 
-    
-    
+
+
     /**
      * @name bindBtns
      * @desc this will bind all the elements to their events
      */
     function bindBtns() {
         /*Initial look of Tabs; Details and Floor Plan*/
-         $("#divDetails").addClass("background-color", "#358016");
-         $("#divDetails").addClass("color", "#FFFFFF");
-       
+        $divDetails.removeClass("notClicked");
         divDetailsClicked();
         divFloorPlanClicked();
     }//bindBtns
-    
-    
+
+
     /**
      * @name init
      * @desc calls other functions, controls the flow
      *
      */
-    function init() {        
+    function init() {
         bindBtns();
+        
     }//end init
 
     /**
@@ -83,5 +90,4 @@
      * @type {init}
      */
     window.onload = init;
-
 })();/*end iffy*/
